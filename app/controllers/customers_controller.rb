d29951bff1ffc593201e1eb668fe5be8a6bf82ba
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-12 17:12:49 -0600

    Revert "Added partial, views, controller actions, css and confiured routes."
    This reverts commit 484af4f9dc750181798a8c18791ca89be5fabf01.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,4 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all
-  end
-
-  def alphabetized
-    @ordered_customers = Customer.all.order(:full_name)
-  end
-
-  def missing_email
-    @missing_email_customers = Customer.where(:email_address => "")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,4 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "customers#index", as: 'root'
-  
-  get 'customers/missing_email', to: 'customers#missing_email', as: 'missing_email'
-
-  get 'customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
-
 end
